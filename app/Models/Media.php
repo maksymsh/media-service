4fcdbd25893fcb1d5898d01ac7a4e0fde1280db9
Maksym Shekhovtsev <maksym.shekhovtsev@gmail.com>
2023-01-29 17:36:11 +0200

    media

---
+++ app/Models/Media.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+/**
+ * @property-read string $uuid
+ * @property-read string $type
+ * @property-read string $extension
+ * @property-read string $humanReadableSize
+ * @property-read string $previewUrl
+ * @property-read string $originalUrl
+ */
+class Media extends \Spatie\MediaLibrary\MediaCollections\Models\Media
+{
+}
