32f4e2bf2b12e4f743561d68f123ceda5ec170e0
Maksym Shekhovtsev <maksym.shekhovtsev@gmail.com>
2023-02-01 09:22:04 +0000

    Update AuthController.php

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -20,7 +20,7 @@ class AuthController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('admin.dashboard'));
+        return redirect()->intended(route('admin.dashboard.index'));
     }
 
     public function logout(Request $request)
