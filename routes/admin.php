e672581307ccb1bb7653cfb4b44cb4414cb70aa5
Maksym Shekhovtsev <maksym.shekhovtsev@gmail.com>
2023-02-01 11:09:56 +0200

    auth

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\BannerController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
