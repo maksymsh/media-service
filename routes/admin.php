fa91e537b8b0bc5df956428476050b38a05c5129
Maksym Shekhovtsev <maksym.shekhovtsev@gmail.com>
2023-02-01 10:55:11 +0200

    auth

--- routes/admin.php
+++ routes/admin.php
@@ -40,36 +40,35 @@ Route::spladeUploads();
 
 Route::prefix($prefix)->as('admin.')->group(function () {
     Route::middleware(['auth'])->group(function () {
+        Route::post('/sorting', [SortingController::class, 'update'])->name('sorting.update');
+        Route::post('/publishing', [PublishingController::class, 'update'])->name('publishing.update');
+
+        // MEDIA LIBRARY
+        Route::post('editor/upload', [UploadController::class, 'editor'])->name('editor.upload');
+        Route::post('media/upload', [UploadController::class, 'mediaStore'])->name('media.store');
+        Route::post('media/order', [UploadController::class, 'mediaOrder'])->name('media.order');
+        Route::patch('media/{media}', [UploadController::class, 'mediaUpdate'])->name('media.update');
+        Route::delete('media/{media}', [UploadController::class, 'mediaDelete'])->name('media.destroy');
+
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
+
+        Route::get('/settings', [SettingsController::class, 'index'])->name('settings.index');
+        Route::post('/settings', [SettingsController::class, 'save']);
+
+        Route::resource('banners', BannerController::class);
+        Route::resource('categories', CategoryController::class);
+        Route::resource('goods', GoodController::class);
+        Route::resource('menus', MenuController::class);
+        Route::resource('menu_items', MenuItemController::class);
+        Route::resource('news', NewsController::class);
+        Route::resource('pages', PageController::class);
+        Route::resource('products', ProductController::class);
+        Route::resource('projects', ProjectController::class);
+        Route::resource('roles', RoleController::class);
+        Route::resource('services', ServiceController::class);
+        Route::resource('studies', StudyController::class);
+        Route::resource('users', UserController::class);
+        Route::resource('vacancies', VacancyController::class);
+        Route::resource('orders', OrderController::class);
     });
-
-    Route::post('/sorting', [SortingController::class, 'update'])->name('sorting.update');
-    Route::post('/publishing', [PublishingController::class, 'update'])->name('publishing.update');
-
-    // MEDIA LIBRARY
-    Route::post('editor/upload', [UploadController::class, 'editor'])->name('editor.upload');
-    Route::post('media/upload', [UploadController::class, 'mediaStore'])->name('media.store');
-    Route::post('media/order', [UploadController::class, 'mediaOrder'])->name('media.order');
-    Route::patch('media/{media}', [UploadController::class, 'mediaUpdate'])->name('media.update');
-    Route::delete('media/{media}', [UploadController::class, 'mediaDelete'])->name('media.destroy');
-
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
-
-    Route::get('/settings', [SettingsController::class, 'index'])->name('settings.index');
-    Route::post('/settings', [SettingsController::class, 'save']);
-
-    Route::resource('banners', BannerController::class);
-    Route::resource('categories', CategoryController::class);
-    Route::resource('goods', GoodController::class);
-    Route::resource('menus', MenuController::class);
-    Route::resource('menu_items', MenuItemController::class);
-    Route::resource('news', NewsController::class);
-    Route::resource('pages', PageController::class);
-    Route::resource('products', ProductController::class);
-    Route::resource('projects', ProjectController::class);
-    Route::resource('roles', RoleController::class);
-    Route::resource('services', ServiceController::class);
-    Route::resource('studies', StudyController::class);
-    Route::resource('users', UserController::class);
-    Route::resource('vacancies', VacancyController::class);
-    Route::resource('orders', OrderController::class);
 });
