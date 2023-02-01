5dafd6de983182ca1452b0d2938d1ad7c0509e35
Maksym Shekhovtsev <maksym.shekhovtsev@gmail.com>
2023-02-01 10:52:19 +0200

    db

--- database/seeders/PageSeeder.php
+++ database/seeders/PageSeeder.php
@@ -14,8 +14,28 @@ class PageSeeder extends Seeder
      */
     public function run()
     {
-        Page::truncate();
+        $uniqueKeys = ['code'];
 
-        Page::factory(10)->create();
+        $data = [
+            Page::factory([
+                'code' => 'home',
+                'title' => 'Home',
+                'url' => '/',
+            ]),
+            Page::factory([
+                'code' => 'about',
+                'title' => 'About us',
+                'url' => '/about-us',
+            ]),
+            Page::factory([
+                'code' => 'contacts',
+                'title' => 'Contacts',
+                'url' => '/contacts',
+            ]),
+        ];
+
+        foreach ($data as $item) {
+            Page::query()->firstOrCreate($item->only($uniqueKeys), $item->getAttributes());
+        }
     }
 }
