48442ff473c26cf9c6a76f34d22229b3b4b17848
Maksym Shekhovtsev <maksym.shekhovtsev@gmail.com>
2023-02-01 10:57:10 +0200

    auth

--- database/migrations/2023_01_29_084141_create_pages_table.php
+++ database/migrations/2023_01_29_084141_create_pages_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('code')->nullable()->unique();
             $table->string('title');
             $table->string('slug')->nullable();
+            $table->string('url')->nullable();
             $table->text('text')->nullable();
 
             $table->string('seo_h1')->nullable();
