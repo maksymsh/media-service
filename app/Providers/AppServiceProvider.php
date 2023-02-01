0ddd1e8ca12db4dee86650fd7ac04f6d070dac40
Maksym Shekhovtsev <maksym.shekhovtsev@gmail.com>
2023-02-01 10:26:18 +0200

    acl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,5 +26,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        if ($this->app->environment('local')) {
+            $superadmin = User::query()->where('username', 'superadmin')->first();
+
+            if ($superadmin) {
+                Auth::login($superadmin);
+            }
+        }
     }
 }
