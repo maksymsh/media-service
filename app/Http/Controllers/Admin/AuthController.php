dc9cf1ecd3cf2f19c609fae29edb2ca6f715cee3
Maksym Shekhovtsev <maksym.shekhovtsev@gmail.com>
2023-02-01 11:05:25 +0200

    auth

---
+++ app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\AuthenticateRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function index()
+    {
+        return view('admin.auth.login');
+    }
+
+    public function login(AuthenticateRequest $request)
+    {
+        $request->authenticate();
+
+        $request->session()->regenerate();
+
+        return redirect()->intended(route('admin.dashboard'));
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->route('admin.login');
+    }
+}
